f88e5b19fa1c85858b0a5d21188668db4e2fc807
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2022-01-16 09:04:04 -0500

    No need to redirect back when create doesn't work
    This changes the behavior so that it renders the new template

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -46,7 +46,7 @@ class ProjectsController < ApplicationController
       redirect_to "/projects"
     else
       flash[:error] = @project.errors.full_messages
-      redirect_back(fallback_location: "projects/new")
+      render :new
     end
   end
 
